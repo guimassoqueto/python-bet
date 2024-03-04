5a162e030bd2aeef7d7515e606a1787d986473fa
guimassoqueto <gui.massoqueto@gmail.com>
2024-03-04 08:51:07 -0300

    chore: add python-dotenv

---
+++ app/config/environment_variables.py
@@ -0,0 +1,12 @@
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+BETANO = os.getenv("BETANO")
+PIXBET = os.getenv("PIXBET")
+ESP_DA_SORTE = os.getenv("ESP_DA_SORTE")
+VAI_DE_BET = os.getenv("VAI_DE_BET")
+ESTRELA_BET = os.getenv("ESTRELA_BET")
+SPORTING_BET = os.getenv("SPORTING_BET")
