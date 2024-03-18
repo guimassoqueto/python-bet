58d565b354dc1600a4da391bafee9f187d582510
guimassoqueto <gui.massoqueto@gmail.com>
2024-03-18 09:13:00 -0300

    chore: update enviroment variables

--- app/config/environment_variables.py
+++ app/config/environment_variables.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 BETANO = os.getenv("BETANO")
-PIXBET = os.getenv("PIXBET")
+PIXBET = os.getenv("PIXBET") # do not work
 ESP_DA_SORTE = os.getenv("ESP_DA_SORTE")
 VAI_DE_BET = os.getenv("VAI_DE_BET")
 ESTRELA_BET = os.getenv("ESTRELA_BET")
