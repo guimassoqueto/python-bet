16722b8a23fedf9c62bab32dc45b9136f53f4be8
guimassoqueto <gui.massoqueto@gmail.com>
2024-03-23 18:09:23 -0300

    feat: update selenium base

--- app/packages/browser/selenium.py
+++ app/packages/browser/selenium.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+import asyncio
+from selenium import webdriver
 
 
-def sync_get_html_content(url: str) -> str:
+async def async_get_html_content(url: str) -> str:
     """
-    Retrieves the HTML content of a given URL.
+    Retrieves the HTML content of a given URL asynchronously.
 
     Args:
         url (str): The URL to retrieve the HTML content from.
@@ -13,9 +15,10 @@ def sync_get_html_content(url: str) -> str:
 
     """
     driver = webdriver.Chrome()
-    driver.get(url)
-    driver.implicitly_wait(15)
+    await asyncio.sleep(0)  # Allow event loop to switch tasks
+    await driver.get(url)
+    await asyncio.sleep(15)  # Wait for 15 seconds
     # TODO: wait for page completely load
-    html_content = driver.page_source
-    driver.quit()
+    html_content = await driver.page_source
+    await driver.quit()
     return html_content
